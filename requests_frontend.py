9d7052c65f5ae2591613f4056f71a44607b1f835
wgh <3030649311@qq.com>
2024-03-08 16:29:44 +0800

    updated requests test

--- requests_frontend.py
+++ requests_frontend.py
@@ -2,11 +2,12 @@ import requests
 
 # "test1.jpg", "img"
 
-url1 = "http://localhost:3001/popup"  # 更改为你的URL
+# url1 = "http://localhost:3001/popup"  # 更改为你的URL
 url2 = "http://localhost:3001/start"  # 更改为你的URL
+# url2 = "http://localhost:3001/stop"  # 更改为你的URL
 
 data = {
-    "source": "http://127.0.0.1:5000/video_feed",  # 图片或图片流的URL
+    "video_url": "http://127.0.0.1:3002/video_feed",  # 图片或图片流的URL
     "type": "stream",  # img或stream
     "event": "打架斗殴事件"  # 事件文字
 }
@@ -17,7 +18,7 @@ data = {
 #     "event": "打架斗殴事件"  # 事件文字
 # }
 
-response = requests.post(url1, json=data)
+response = requests.post(url2, json=data)
 # response = requests.post(url2)
 
 print(response.status_code)
